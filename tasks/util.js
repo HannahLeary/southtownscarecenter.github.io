84c2862d3584675461f87660dbcddfc13230f274
Shannon Moeller <me@shannonmoeller.com>
2016-06-05 00:16:31 -0400

    Revamp.

--- tasks/util.js
+++
@@ -1,9 +0,0 @@
-export function debounce(fn, delay = 400) {
-	return function debounced() {
-		if (debounced.timeout) {
-			clearTimeout(debounced.timeout);
-		}
-
-		debounced.timeout = setTimeout(fn, delay);
-	};
-}
