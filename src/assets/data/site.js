3b869cf225445ea3f8e245e92c892c2acbf567a2
Shannon Moeller <me@shannonmoeller.com>
2016-07-05 15:15:14 -0400

    Content updates.

--- src/assets/data/site.js
+++ src/assets/data/site.js
@@ -2,3 +2,4 @@ export const canonical = 'http://southtownscarecenter.org';
 export const title = 'Southtowns Care Center';
 export const analyticsId = 'UA-66801390-1';
 export const socialId = 'ra-55de798ed2afb72f';
+export const mapsToken = 'AIzaSyCF4OSB36v8qFMDYAHzHKIxRVhj99fIIfQ';
