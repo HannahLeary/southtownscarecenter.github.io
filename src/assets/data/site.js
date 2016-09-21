83bfe1327698a166b5af4e6e9c6727dd77c4cfd0
Shannon Moeller <me@shannonmoeller.com>
2016-09-20 21:24:54 -0400

    Added board bios and copyright year.

--- src/assets/data/site.js
+++ src/assets/data/site.js
@@ -3,3 +3,4 @@ export const title = 'Southtowns Care Center';
 export const analyticsId = 'UA-66801390-1';
 export const socialId = 'ra-55de798ed2afb72f';
 export const mapsToken = 'AIzaSyCF4OSB36v8qFMDYAHzHKIxRVhj99fIIfQ';
+export const year = new Date().getFullYear();
